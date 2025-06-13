0b21092f2e00bb0edb9ded411ce9757daf217d75
kosukesando <git@ksando.com>
2025-06-13 13:36:35 +0800

    flat loops

--- src/OW3DHelper.jl
+++ src/OW3DHelper.jl
@@ -181,20 +181,18 @@ function calc_eta(oi, kxmatg, kymatg, ωmatg, t, ampg_newwave_norm)
     xvec = oi.dx * (-(oi.nx - 1)/2:1:(oi.nx-1)/2)
     yvec = oi.dy * (-(oi.ny - 1)/2:1:(oi.ny-1)/2)
     # Calculate linear free surface
-    println("Calculating linear free surface")
-    for kj = eachindex(1:nky)
-        for ki = eachindex(1:nkx)
-            local kx = kxmatg[ki, kj]
-            local ky = kymatg[ki, kj]
-            local ω = ωmatg[ki, kj]
-            local an = ampg_newwave_norm[ki, kj]
-            for yi = eachindex(yvec), xi = eachindex(xvec)
-                local x = xvec[xi]
-                local y = yvec[yi]
-                local phasei = kx * x + ky * y - ω * t + deg2rad(oi.ϕ)
-                local etacomp = an * cos(phasei)
-                η[xi, yi] += etacomp
-            end
+    println("Calculating linear free surface(serial)")
+    for kj = eachindex(1:nky), ki = eachindex(1:nkx)
+        local kx = kxmatg[ki, kj]
+        local ky = kymatg[ki, kj]
+        local ω = ωmatg[ki, kj]
+        local an = ampg_newwave_norm[ki, kj]
+        for yi = eachindex(yvec), xi = eachindex(xvec)
+            local x = xvec[xi]
+            local y = yvec[yi]
+            local phasei = kx * x + ky * y - ω * t + deg2rad(oi.ϕ)
+            local etacomp = an * cos(phasei)
+            η[xi, yi] += etacomp
         end
     end
     return η
