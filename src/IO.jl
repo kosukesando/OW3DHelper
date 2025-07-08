2628af55848caa4c0dd3fd703fc6165d5f4e7f2a
Kosuke Sando <kosukesando@gmail.com>
2025-07-08 16:04:09 +0800

    cleanup and organize

---
+++ src/IO.jl
@@ -0,0 +1,134 @@
+module OW3DIO
+
+using Printf
+
+export
+    export_ow3d_init,
+    export_ow3d_inp,
+    open_EP,
+    generate_init
+
+include("./OW3DHelper.jl")
+using .OW3DHelper
+
+function export_ow3d_init(η, ϕ, stime, oi::OW3DInput, dir; include_param=true)
+    println("generating initial file...")
+    if include_param
+        file_name = "OceanWave3D_$(oi.nx)x$(oi.ny)_$(round(Int,100*oi.A))cm_rot$(round(Int,oi.twist_angle))_phase$(round(Int,oi.ϕ))_depth$(round(Int,oi.depth))_mwd$(round(Int,oi.mwd)).init"
+    else
+        file_name = "OceanWave3D.init"
+    end
+    Lx = (oi.nx - 1) * oi.dx
+    Ly = (oi.ny - 1) * oi.dy
+    fpath = joinpath(dir, file_name)
+    open(fpath, "w") do file
+        write(file, @sprintf "Tropical Cyclone focus wave H=%0.3f nx=%d ny=%d dx=%0.3f dy=%0.3f depth=%0.3f phase=%0.3f Twist=%0.3f MWD=%0.3f" oi.A oi.nx oi.ny oi.dx oi.dy oi.depth oi.ϕ oi.twist_angle oi.mwd)
+        write(file, @sprintf "\n%12e %12e %d %d %12e" Lx Ly oi.nx oi.ny stime)
+        for ry = 1:oi.ny
+            for rx = 1:oi.nx
+                write(file, @sprintf "\n%12e %12e" η[rx, ry] ϕ[rx, ry])
+            end
+        end
+    end
+end
+
+function generate_init(A::Float64, ϕ::Float64, k0::Float64, kmaxx::Float64, kmaxy::Float64, depth::Float64, dx::Float64, dy::Float64, nx::Int, ny::Int, spectrum::String, spreading_type::String, spreading_param::Float64, twist_angle::Float64, mwd::Float64, t::Float64; twist_type::String="mcallister", γ=3.3, dir=".")
+    if spectrum == "JONSWAP"
+        spec = JSpec(γ)
+    end
+
+    oi = OW3DInput(A, ϕ, k0, kmaxx, kmaxy, depth, dx, dy, nx, ny, spec, spreading_type, spreading_param, twist_angle, mwd, twist_type)
+    eta, phi = calc_etaphi(oi, t)
+    export_ow3d_init(eta, phi, 0, oi, dir; include_param=false)
+end
+
+function export_ow3d_inp(oi::OW3DInput, nt, kinematics, dir; include_param=true, nz=9, di_kine=5, dt=0.2)
+    println("generating input file...")
+    if include_param
+        file_name = "OceanWave3D_$(oi.nx)x$(oi.ny)_$(round(Int,100*oi.A))cm_rot$(round(Int,oi.twist_angle))_phase$(round(Int,oi.ϕ))_depth$(round(Int,oi.depth))_mwd$(round(Int,oi.mwd)).inp"
+    else
+        file_name = "OceanWave3D.inp"
+    end
+    Lx = (oi.nx - 1) * oi.dx
+    Ly = (oi.ny - 1) * oi.dy
+    fpath = joinpath(dir, file_name)
+    open(fpath, "w") do file
+        write(file, "A flat bottom, focused wave initial condition in 3D\n")
+        write(
+            file,
+            "-1 2 0.5                                 <- Initial condition (0=defined by funPressureTerm.f90, 1=NL standing wave, 2=shoaling on a smooth beach, 3=Whalin bar, ... see Initialization.f90:SetupInitialConditions); IncWaveType (0=none, 1=stream function, 2=linear regular or irregular waves)\n"
+        )
+        write(file, @sprintf "%0.3f %0.3f %0.3f %d %d %d 0 0 1 1 1 1    <- Lx Ly Lz Nx Ny Nz GridX GridY GridZ(0=even,1=clustering) GhostGrid (0=off,1=on)\n" Lx Ly oi.depth oi.nx oi.ny nz)
+        write(
+            file,
+            "3 3 3 1 1 1                           <- alpha, beta, gamma\n"
+        )
+        write(file, @sprintf "%d %0.3f 1 0 1                       <- Nsteps, dt, timeintegration scheme (1=RK4,2=lowstorage-RK45), CFL (if CFL/=0 then dt=CFL*dxmin/c, assume c=sqrt(g*hdeep)), RK4-ExtrapolationON/OFF\n" nt dt)
+        write(
+            file,
+            "9.81                                  <- gravitational acceleration constant\n"
+        )
+        write(
+            file,
+            "1 3 0 55 1e-6 1e-6 1 V 1 1 20          <- solver (0:DC, 1:GMRES), GMRES Preconditioning (0=none (Matrix free,DIRECT),1=Linear LU(no elimination),2=Linear LU(ghostpoints eliminated),3=Multigrid (no elimination) ), Coarsening Strategy (0=Allan's, 1=Ole's), GMRESmaxiterations, relative tolerance (reltol), abstol, maxit, cyclet, pre-smoothings, post-smoothings, MGmaxgrids, DOF breakeven\n"
+        )
+        write(
+            file,
+            "0.05 1.00 1.84 2 0 0 1 6 32           <- Stream function solution parameters: H, h, L, T, WAVELorPER, uEorS, EorS, nsteps, maxiter \n"
+        )
+        write(file, @sprintf "%d 20 1 %d         <- StoreDataOnOff, formattype, (StoreDataOnOff=0 -> no output, StoreDataOnOff=+stride-> binary, StoreDataOnOff=-stride -> ascii every stride time steps.  formattype=0, binary; =1, unformatted) If formattype=20, then the line should read: StoreDataOnOff, iKinematics, formattype, nOutFiles; and nOutFiles lines should appear below defining  [xbeg, xend, xstride, ybeg, yend, ystride, tbeg, tend, tstride] for each file.\n" di_kine length(kinematics))
+        for kinematic in kinematics
+            write(file, @sprintf "%d %d %d %d %d %d %d %d %d   <- xbeg, xend, xstride, ybeg, yend, ystride, tbeg, tend, tstride\n" kinematic.xbeg kinematic.xend kinematic.xstride kinematic.ybeg kinematic.yend kinematic.ystride kinematic.tbeg kinematic.tend kinematic.tstride)
+        end
+        write(file, " \n")
+        write(
+            file,
+            "1 0                                   <- 0/1=linear/nonlinear computations, Dynamic pressure term on/off\n"
+        )
+        write(
+            file,
+            "0 6 10 0.08 0.08 0.4                  <- SG-filtering on/off, filter half width, poly order\n"
+        )
+        write(
+            file,
+            "0 8. 3 Y 0.0                          <- relaxation zones on/off, transient time, no. zones. For each zone define on following lines: x1 x2 y1 y2 ftype(=relaxation function) param XorY WavegenONOFF Degrees(=IC rotation)\n"
+        )
+        write(
+            file,
+            "0 0                                   <- Damping pressure zone:  PDampingOnOff=0 (off), number of zones.  For each zone include the line: x1, x2, y1, y2 (bounds of the zone), gamma0 (dynamic FSBC), Gamma0 (kinematic FSBC), i_damperType (0=friction on the velocity, 1=friction on the potential).\n"
+        )
+        write(
+            file,
+            "0 2.0 2 0 0 1 0                       <- SWENSE on/off, ramp in time, wf direction (1:+x ; -1:-x ; 2:+y ; -2:-y ; >3: angle of the 3D wavefield), Reflexion of incident wf: West, East, North, South (0=off,1=on)\n"
+        )
+        write(
+            file,
+            "0                                     <- Curvilinear on/off \n"
+        )
+        write(
+            file,
+            "33  8. 2. 80. 20. -1 -11 100. 50. run06.el 22.5 1.0 3.3  <- Irregular/regular waves:  i_spec, T_p, H_s, h0, kh_max, seed, seed2, x0, y0, (inc_wave_file or gamma_JONSWAP, if ispec=2 or 3), (inc_wave_file,beta,S,gamma_JONSWAP if ispec>=30).  For a random wave, the spectrum (i_spec=):  -1=>Monochromatic, 0=>P-M, 1=>JONSWAP, 2=>Read from a file, 3=>JONSWAP with input gamma; +- 3* means 3D at angle beta -30=>Monochromatic, 30=>P-M, 31=>JONSWAP, 32=>Not yet implemented 33=>JONSWAP with Normal spreading, 34=> JONSWAP with cos^S spreading.  \n"
+        )
+    end
+end
+
+function open_EP(fpath)
+    io_ep = open(fpath)
+    seek(io_ep, sizeof(Int32))
+    Nx = read(io_ep, Int32)
+    Ny = read(io_ep, Int32)
+    seek(io_ep, sizeof(Int32) * 5)
+    X = Array{Float64}(undef, Nx * Ny)
+    Y = Array{Float64}(undef, Nx * Ny)
+    read!(io_ep, X)
+    read!(io_ep, Y)
+    E = Array{Float64}(undef, Nx * Ny)
+    P = Array{Float64}(undef, Nx * Ny)
+    read!(io_ep, E)
+    read!(io_ep, P)
+    close(io_ep)
+    E = reshape(E, Nx, Ny)[2:end-1, 2:end-1]
+    P = reshape(P, Nx, Ny)[2:end-1, 2:end-1]
+    EPFile(Nx - 2, Ny - 2, reshape(X, Nx, Ny)[2:end-1, 2:end-1], reshape(Y, Nx, Ny)[2:end-1, 2:end-1], E, P)
+end
+end
--- src/OW3DHelper.jl
+++ src/OW3DHelper.jl
@@ -1,6 +1,8 @@
 module OW3DHelper
+
 # using SpecialFunctions
 using Printf
+using FFTW
 using OhMyThreads: @tasks, @local, @set
 
 export
@@ -8,11 +10,7 @@ export
     EPFile,
     calc_etaphi,
     JSpec,
-    KinematicSetting,
-    export_ow3d_init,
-    export_ow3d_inp,
-    open_EP,
-    generate_init
+    KinematicSetting
 
 const g::Float64 = 9.81
 
@@ -166,18 +164,23 @@ function calc_k_omega_a(oi)
 
     ampg_newwave_norm = oi.A * (ampg_newwave ./ sum(ampg_newwave)) #  normalise to control size of event
 
-    kxmatg, kymatg, ωmatg, ampg_newwave_norm
+    kxmatg, kymatg, ωmatg, ampg_newwave_norm, dirg
 end
 
-function calc_etaphi(oi::OW3DInput, t::Number)
-    kxmatg, kymatg, ωmatg, ampg_newwave_norm = calc_k_omega_a(oi)
+function calc_etaphi(oi::OW3DInput, t::Number; order=1)
+    kxmatg, kymatg, ωmatg, ampg_newwave_norm, dirg = calc_k_omega_a(oi)
     η = calc_eta(oi, kxmatg, kymatg, ωmatg, t, ampg_newwave_norm)
     ϕ = calc_phi(oi, kxmatg, kymatg, ωmatg, t, ampg_newwave_norm, η)
-    η, ϕ
+    if order == 1
+        return η, ϕ
+    elseif order == 2
+        η₂₂, ϕ₂₂, η₂₀, ϕ₂₀ = calc_sec_etaphi(oi, kxmatg, kymatg, ωmatg, t, ampg_newwave_norm, dirg)
+        return η, ϕ, η₂₂, ϕ₂₂, η₂₀, ϕ₂₀
+    end
 end
 
 function calc_etaphi(oi::OW3DInput, ts::Int, te::Int, x, y)
-    kxmatg, kymatg, ωmatg, ampg_newwave_norm = calc_k_omega_a(oi)
+    kxmatg, kymatg, ωmatg, ampg_newwave_norm, dirg = calc_k_omega_a(oi)
     t = ts:te
     η = calc_eta(oi, kxmatg, kymatg, ωmatg, t, ampg_newwave_norm, x, y)
     # ϕ = calc_phi(oi, kxmatg, kymatg, ωmatg, t, ampg_newwave_norm, x, y)
@@ -246,125 +249,168 @@ function calc_etaphi_origin(oi::OW3DInput, ts::Int, te::Int)
     calc_etaphi(oi, ts, te, 0.0, 0.0)
 end
 
-function export_ow3d_init(η, ϕ, stime, oi::OW3DInput, dir; include_param=true)
-    println("generating initial file...")
-    if include_param
-        file_name = "OceanWave3D_$(oi.nx)x$(oi.ny)_$(round(Int,100*oi.A))cm_rot$(round(Int,oi.twist_angle))_phase$(round(Int,oi.ϕ))_depth$(round(Int,oi.depth))_mwd$(round(Int,oi.mwd)).init"
-    else
-        file_name = "OceanWave3D.init"
-    end
-    Lx = (oi.nx - 1) * oi.dx
-    Ly = (oi.ny - 1) * oi.dy
-    fpath = joinpath(dir, file_name)
-    open(fpath, "w") do file
-        write(file, @sprintf "Tropical Cyclone focus wave H=%0.3f nx=%d ny=%d dx=%0.3f dy=%0.3f depth=%0.3f phase=%0.3f Twist=%0.3f MWD=%0.3f" oi.A oi.nx oi.ny oi.dx oi.dy oi.depth oi.ϕ oi.twist_angle oi.mwd)
-        write(file, @sprintf "\n%12e %12e %d %d %12e" Lx Ly oi.nx oi.ny stime)
-        for ry = 1:oi.ny
-            for rx = 1:oi.nx
-                write(file, @sprintf "\n%12e %12e" η[rx, ry] ϕ[rx, ry])
+function calc_sec_etaphi(oi::OW3DInput, kxmatg, kymatg, ωmatg, t, ampg_newwave_norm, dirg)
+    nx = oi.nx
+    ny = oi.ny
+    phase = deg2rad(oi.phase)
+    nkx = nothing
+    nky = nothing
+    kxi = nothing
+    kyi = nothing
+    depth = oi.depth
+    kmatg = sqrt.(kxmatg .^ 2 .+ kymatg .^ 2)
+
+    C22 = zeros(2 * (nx - 1) + 1, 2 * (nx - 1) + 1)
+    V22 = zeros(2 * (nx - 1) + 1, 2 * (nx - 1) + 1)
+    C20 = zeros(2 * (nx - 1) + 1, 2 * (nx - 1) + 1)
+    V20 = zeros(2 * (nx - 1) + 1, 2 * (nx - 1) + 1)
+
+    ath = 10^(-6) # amplitude threshold for calculation of second order interactions
+
+    #First calculate self interaction term.
+
+    println("Calculating self interaction term")
+    maxes = 0
+
+    # for q00=1:1:length(iia)
+    for i in 1:nkx
+        for j in 1:nky
+            if ampg_newwave_norm[i, j] < ath
+                continue
             end
+            e2sx = argmin(abs(kxi .- (kxmatg[i, j] + kxmatg[i, j])))
+            e2sy = argmin(abs(kyi .- (kymatg[i, j] + kymatg[i, j])))
+            iix2s = minimum(abs(kxi .- (kxmatg[i, j] + kxmatg[i, j])))
+            iiy2s = minimum(abs(kyi .- (kymatg[i, j] + kymatg[i, j])))
+
+            maxes = max([maxes e2sx e2sy])
+
+            if maxes > (10^-12)
+                "warning: misassigned component"
+            end
+
+            as2s = ((ampg_newwave_norm[i, j]^2) * kmatg[i, j] / (4 * tanh(kmatg[i, j] * depth))) * (2 + 3 / ((sinh(kmatg[i, j] * depth))^2))
+            av2s = ((ampg_newwave_norm[i, j]^2) * (3 * ωmatg[i, j] / 8) * cosh(2 * kmatg[i, j] * depth) / ((sinh(kmatg[i, j] * depth))^4))
+            p2s = (phase - t * ωmatg[i, j]) + (phase - t * ωmatg[i, j])
+            c2s = complex(as2s * cos(p2s), as2s * sin(p2s))
+            v2s = complex(av2s * cos(p2s - pi / 2), av2s * sin(p2s - pi / 2))
+
+            C22[iiy2s, iix2s] = C22[iiy2s, iix2s] + c2s
+            V22[iiy2s, iix2s] = V22[iiy2s, iix2s] + v2s
         end
     end
-end
 
-function generate_init(A::Float64, ϕ::Float64, k0::Float64, kmaxx::Float64, kmaxy::Float64, depth::Float64, dx::Float64, dy::Float64, nx::Int, ny::Int, spectrum::String, spreading_type::String, spreading_param::Float64, twist_angle::Float64, mwd::Float64, t::Float64; twist_type::String="mcallister", γ=3.3, dir=".")
-    if spectrum == "JONSWAP"
-        spec = JSpec(γ)
-    end
+    #Now calculate the interaction between each component
 
-    oi = OW3DInput(A, ϕ, k0, kmaxx, kmaxy, depth, dx, dy, nx, ny, spec, spreading_type, spreading_param, twist_angle, mwd, twist_type)
-    eta, phi = calc_etaphi(oi, t)
-    export_ow3d_init(eta, phi, 0, oi, dir; include_param=false)
-end
+    println("Calculating second order cross-interactions...")
 
-function export_ow3d_inp(oi::OW3DInput, nt, kinematics, dir; include_param=true, nz=9, di_kine=5, dt=0.2)
-    println("generating input file...")
-    if include_param
-        file_name = "OceanWave3D_$(oi.nx)x$(oi.ny)_$(round(Int,100*oi.A))cm_rot$(round(Int,oi.twist_angle))_phase$(round(Int,oi.ϕ))_depth$(round(Int,oi.depth))_mwd$(round(Int,oi.mwd)).inp"
-    else
-        file_name = "OceanWave3D.inp"
-    end
-    Lx = (oi.nx - 1) * oi.dx
-    Ly = (oi.ny - 1) * oi.dy
-    fpath = joinpath(dir, file_name)
-    open(fpath, "w") do file
-        write(file, "A flat bottom, focused wave initial condition in 3D\n")
-        write(
-            file,
-            "-1 2 0.5                                 <- Initial condition (0=defined by funPressureTerm.f90, 1=NL standing wave, 2=shoaling on a smooth beach, 3=Whalin bar, ... see Initialization.f90:SetupInitialConditions); IncWaveType (0=none, 1=stream function, 2=linear regular or irregular waves)\n"
-        )
-        write(file, @sprintf "%0.3f %0.3f %0.3f %d %d %d 0 0 1 1 1 1    <- Lx Ly Lz Nx Ny Nz GridX GridY GridZ(0=even,1=clustering) GhostGrid (0=off,1=on)\n" Lx Ly oi.depth oi.nx oi.ny nz)
-        write(
-            file,
-            "3 3 3 1 1 1                           <- alpha, beta, gamma\n"
-        )
-        write(file, @sprintf "%d %0.3f 1 0 1                       <- Nsteps, dt, timeintegration scheme (1=RK4,2=lowstorage-RK45), CFL (if CFL/=0 then dt=CFL*dxmin/c, assume c=sqrt(g*hdeep)), RK4-ExtrapolationON/OFF\n" nt dt)
-        write(
-            file,
-            "9.81                                  <- gravitational acceleration constant\n"
-        )
-        write(
-            file,
-            "1 3 0 55 1e-6 1e-6 1 V 1 1 20          <- solver (0:DC, 1:GMRES), GMRES Preconditioning (0=none (Matrix free,DIRECT),1=Linear LU(no elimination),2=Linear LU(ghostpoints eliminated),3=Multigrid (no elimination) ), Coarsening Strategy (0=Allan's, 1=Ole's), GMRESmaxiterations, relative tolerance (reltol), abstol, maxit, cyclet, pre-smoothings, post-smoothings, MGmaxgrids, DOF breakeven\n"
-        )
-        write(
-            file,
-            "0.05 1.00 1.84 2 0 0 1 6 32           <- Stream function solution parameters: H, h, L, T, WAVELorPER, uEorS, EorS, nsteps, maxiter \n"
-        )
-        write(file, @sprintf "%d 20 1 %d         <- StoreDataOnOff, formattype, (StoreDataOnOff=0 -> no output, StoreDataOnOff=+stride-> binary, StoreDataOnOff=-stride -> ascii every stride time steps.  formattype=0, binary; =1, unformatted) If formattype=20, then the line should read: StoreDataOnOff, iKinematics, formattype, nOutFiles; and nOutFiles lines should appear below defining  [xbeg, xend, xstride, ybeg, yend, ystride, tbeg, tend, tstride] for each file.\n" di_kine length(kinematics))
-        for kinematic in kinematics
-            write(file, @sprintf "%d %d %d %d %d %d %d %d %d   <- xbeg, xend, xstride, ybeg, yend, ystride, tbeg, tend, tstride\n" kinematic.xbeg kinematic.xend kinematic.xstride kinematic.ybeg kinematic.yend kinematic.ystride kinematic.tbeg kinematic.tend kinematic.tstride)
+    maxec = 0
+
+    for ij1 in 1:nkx*nky-1
+        i1 = 1 + (ij1 - 1) % nky
+        j1 = 1 + (ij1 - 1) ÷ nky
+        sinh1squared = (sinh(kmatg[i1, j1] * depth))^2
+        tanh1 = tanh(kmatg[i1, j1] * depth)
+        for ij2 in ij1+1:nkx*nky
+            i2 = 1 + (ij2 - 1) % nky
+            j2 = 1 + (ij2 - 1) ÷ nky
+            sinh2squared = (sinh(kmatg[i2, j2] * depth))^2
+            tanh2 = tanh(kmatg[i2, j2] * depth)
+            cosdt = cosd(dirg[i1, j1] - dirg[i2, j2]) # cos of the angle between the constituents
+            pmodk = sqrt((kxmatg[i1, j1] + kxmatg[i2, j2])^2 + (kymatg[i1, j1] + kymatg[i2, j2])^2)  #mod(k1+k2)
+            mmodk = sqrt((kxmatg[i1, j1] - kxmatg[i2, j2])^2 + (kymatg[i1, j1] - kymatg[i2, j2])^2)  #mod(k1-k2)
+            Dp = (ωmatg[i1, j1] + ωmatg[i2, j2])^2 - g * pmodk * tanh(pmodk * depth) #Equation 21 in Dalzell
+            Dm = (ωmatg[i1, j1] - ωmatg[i2, j2])^2 - g * mmodk * tanh(mmodk * depth) #Equation 22 in Dalzell
+            tanhs = tanh1 * tanh2
+            if tanhs < 0
+                println("Alert! tanhs=$(tanhs)")
+            end
+            Ap1 = -(ωmatg[i1, j1] * ωmatg[i2, j2] * (ωmatg[i1, j1] + ωmatg[i2, j2]) / Dp) * (1 - cosdt / tanhs) #First term in eq 17 of Dalzell
+            Ap2 = (1 / (2 * Dp)) * ((ωmatg[i1, j1]^3) / sinh1squared + (ωmatg[i2, j2]^3) / sinh2squared) #Second term in eq 17 of Dalzell
+            Ap = Ap1 + Ap2
+            Am1 = (ωmatg[i1, j1] * ωmatg[i2, j2] * (ωmatg[i1, j1] - ωmatg[i2, j2]) / Dm) * (1 + cosdt / tanhs) #First term in eq 18 of Dalzell
+            Am2 = (1 / (2 * Dm)) * ((ωmatg[i1, j1]^3) / sinh1squared - (ωmatg[i2, j2]^3) / sinh2squared) #Second term in eq 18 of Dalzell
+            Am = Am1 + Am2
+            Bp0 = (ωmatg[i1, j1]^2 + ωmatg[i2, j2]^2) / (2 * g) #First term in 19 and 20 of Dalzell
+            Bp1 = -((ωmatg[i1, j1] * ωmatg[i2, j2]) / (2 * g)) * (1 - cosdt / tanhs) * ((ωmatg[i1, j1] + ωmatg[i2, j2])^2 + g * pmodk * tanh(pmodk * depth)) / Dp #Second term in eq 19
+            Bp2 = ((ωmatg[i1, j1] + ωmatg[i2, j2]) / (2 * g * Dp)) * (((ωmatg[i1, j1]^3) / sinh1squared) + ((ωmatg[i2, j2]^3) / sinh2squared)) #Third term in eq 19
+            Bm1 = ((ωmatg[i1, j1] * ωmatg[i2, j2]) / (2 * g)) * (1 + cosdt / tanhs) * ((ωmatg[i1, j1] - ωmatg[i2, j2])^2 + g * mmodk * tanh(mmodk * depth)) / Dm #Second term in eq 20
+            Bm2 = ((ωmatg[i1, j1] - ωmatg[i2, j2]) / (2 * g * Dm)) * (((ωmatg[i1, j1]^3) / sinh1squared) - ((ωmatg[i2, j2]^3) / sinh2squared)) #Third term in eq 19
+            Bp = Bp0 + Bp1 + Bp2
+            Bm = Bp0 + Bm1 + Bm2
+
+            e2px = minimum(abs(kxi .- (kxmatg[i1, j1] + kxmatg[i2, j2])))
+            e2py = minimum(abs(kyi .- (kymatg[i1, j1] + kymatg[i2, j2])))
+            e2mx = minimum(abs(kxi .- (kxmatg[i2, j2] - kxmatg[i1, j1])))
+            e2my = minimum(abs(kyi .- (kymatg[i2, j2] - kymatg[i1, j1])))
+            iix2p = argmin(abs(kxi .- (kxmatg[i1, j1] + kxmatg[i2, j2])))
+            iiy2p = argmin(abs(kyi .- (kymatg[i1, j1] + kymatg[i2, j2])))
+            iix2m = argmin(abs(kxi .- (kxmatg[i2, j2] - kxmatg[i1, j1])))
+            iiy2m = argmin(abs(kyi .- (kymatg[i2, j2] - kymatg[i1, j1])))
+
+            maxec = maximum([maxec, e2px, e2py, e2mx, e2my])
+
+            if maxec > (10^-12)
+                println("warning: misassigned component")
+            end
+
+            a22 = ampg_newwave_norm[i1, j1] * ampg_newwave_norm[i2, j2]
+            p22 = (phase - t * ωmatg[i1, j1]) + (phase - t * ωmatg[i2, j2])
+            c22 = complex(a22 * Bp * cos(p22), a22 * Bp * sin(p22))
+            v22 = complex(a22 * Ap * cos(p22 - pi / 2), a22 * Ap * sin(p22 - pi / 2))
+
+            a20 = ampg_newwave_norm[i1, j1] * ampg_newwave_norm[i2, j2]
+            p20 = (phase - t * ωmatg[i1, j1]) - (phase - t * ωmatg[i2, j2])
+            c20 = complex(a20 * Bm * cos(p20), a20 * Bm * sin(p20))
+            v20 = complex(a20 * Am * cos(p20 - pi / 2), a20 * Am * sin(p20 - pi / 2))
+
+            C22[iiy2p, iix2p] = C22(iiy2p, iix2p) + c22
+            V22[iiy2p, iix2p] = V22(iiy2p, iix2p) + v22
+            C20[iiy2m, iix2m] = C20(iiy2m, iix2m) + c20
+            V20[iiy2m, iix2m] = V20(iiy2m, iix2m) + v20
         end
-        write(file, " \n")
-        write(
-            file,
-            "1 0                                   <- 0/1=linear/nonlinear computations, Dynamic pressure term on/off\n"
-        )
-        write(
-            file,
-            "0 6 10 0.08 0.08 0.4                  <- SG-filtering on/off, filter half width, poly order\n"
-        )
-        write(
-            file,
-            "0 8. 3 Y 0.0                          <- relaxation zones on/off, transient time, no. zones. For each zone define on following lines: x1 x2 y1 y2 ftype(=relaxation function) param XorY WavegenONOFF Degrees(=IC rotation)\n"
-        )
-        write(
-            file,
-            "0 0                                   <- Damping pressure zone:  PDampingOnOff=0 (off), number of zones.  For each zone include the line: x1, x2, y1, y2 (bounds of the zone), gamma0 (dynamic FSBC), Gamma0 (kinematic FSBC), i_damperType (0=friction on the velocity, 1=friction on the potential).\n"
-        )
-        write(
-            file,
-            "0 2.0 2 0 0 1 0                       <- SWENSE on/off, ramp in time, wf direction (1:+x ; -1:-x ; 2:+y ; -2:-y ; >3: angle of the 3D wavefield), Reflexion of incident wf: West, East, North, South (0=off,1=on)\n"
-        )
-        write(
-            file,
-            "0                                     <- Curvilinear on/off \n"
-        )
-        write(
-            file,
-            "33  8. 2. 80. 20. -1 -11 100. 50. run06.el 22.5 1.0 3.3  <- Irregular/regular waves:  i_spec, T_p, H_s, h0, kh_max, seed, seed2, x0, y0, (inc_wave_file or gamma_JONSWAP, if ispec=2 or 3), (inc_wave_file,beta,S,gamma_JONSWAP if ispec>=30).  For a random wave, the spectrum (i_spec=):  -1=>Monochromatic, 0=>P-M, 1=>JONSWAP, 2=>Read from a file, 3=>JONSWAP with input gamma; +- 3* means 3D at angle beta -30=>Monochromatic, 30=>P-M, 31=>JONSWAP, 32=>Not yet implemented 33=>JONSWAP with Normal spreading, 34=> JONSWAP with cos^S spreading.  \n"
-        )
     end
-end
 
-function open_EP(fpath)
-    io_ep = open(fpath)
-    seek(io_ep, sizeof(Int32))
-    Nx = read(io_ep, Int32)
-    Ny = read(io_ep, Int32)
-    seek(io_ep, sizeof(Int32) * 5)
-    X = Array{Float64}(undef, Nx * Ny)
-    Y = Array{Float64}(undef, Nx * Ny)
-    read!(io_ep, X)
-    read!(io_ep, Y)
-    E = Array{Float64}(undef, Nx * Ny)
-    P = Array{Float64}(undef, Nx * Ny)
-    read!(io_ep, E)
-    read!(io_ep, P)
-    close(io_ep)
-    E = reshape(E, Nx, Ny)[2:end-1, 2:end-1]
-    P = reshape(P, Nx, Ny)[2:end-1, 2:end-1]
-    EPFile(Nx - 2, Ny - 2, reshape(X, Nx, Ny)[2:end-1, 2:end-1], reshape(Y, Nx, Ny)[2:end-1, 2:end-1], E, P)
+
+    # Construct Fourier Components corresponding to second order
+
+    F22 = 0.5 * prod(size(C22)) * ifftshift(ifftshift(C22, 1), 2)
+    E22 = circshift(
+        ifftshift(
+            ifftshift(
+                irfft(F22, (nx, ny)),
+                1),
+            2),
+        (-1, -1))
+
+    G22 = 0.5 * prod(size(V22)) * ifftshift(ifftshift(V22, 1), 2)
+    P22 = circshift(
+        ifftshift(
+            ifftshift(
+                irfft(G22, (nx, ny)),
+                1),
+            2),
+        (-1, -1))
+
+    F20 = 0.5 * prod(size(C20)) * ifftshift(ifftshift(C20, 1), 2)
+    E20 = circshift(
+        ifftshift(
+            ifftshift(
+                irfft(F20, (nx, ny)),
+                1),
+            2),
+        (-1, -1))
+
+    G20 = 0.5 * prod(size(V20)) * ifftshift(ifftshift(V20, 1), 2)
+    P20 = reverse(circshift(
+            ifftshift(
+                ifftshift(
+                    irfft(G20, (nx, ny)),
+                    1),
+                2),
+            (-1, -1)),
+        1)
+    return E22, P22, E20, P20
 end
 
 end
