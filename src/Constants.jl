71693a52b67527b9d0d59f03ae020eca5aef3553
kosukesando <git@ksando.com>
2025-08-25 16:44:16 +0800

    New plotting helper

--- src/Constants.jl
+++ src/Constants.jl
@@ -1,1 +1,4 @@
 const g::Float64 = 9.81
+const inch = 96
+const pt = 4 / 3
+const cm = inch / 2.54
---
+++ src/Video.jl
@@ -0,0 +1,113 @@
+function plot_raw2d(s, var; basedir=".", xlim=(-1000, 1000), ylim=(-1000, 1000))
+    @assert var in ["eta", "phi"]
+    dir = joinpath(basedir, @sprintf("%s/%03ddeg", s.casename, s.twist))
+    mktempdir() do tempdir
+        Threads.@threads for i in 1:s.N
+            local fname = joinpath(tempdir, @sprintf("2d_%03d.png", i))
+            local v = NCDataset(joinpath(dir, "ep.nc"))[@sprintf("%s%03d", var, s.phase)][i, :, :]
+            local f = Figure(size=(11cm, 8cm), fontsize=12pt)
+            local ax = Axis(
+                f[1, 1],
+                title="Time=$(i)s",
+                xlabel="x(m)",
+                ylabel="y(m)",
+            )
+            colsize!(f.layout, 1, Aspect(1, 1.0))
+            xlims!(ax, xlim)
+            ylims!(ax, ylim)
+            local im = image!(
+                ax,
+                (-s.Lx / 2, s.Lx / 2),
+                (-s.Ly / 2, s.Ly / 2),
+                v,
+                colorrange=(-s.amp * 1.2, s.amp * 1.2),
+                colormap=:seismic,
+                highclip=:black,
+            )
+            Colorbar(f[1, 2], im)
+            save(fname, f)
+        end
+        output = joinpath(dir, @sprintf("video/%s_2d_%03d.mp4", var, s.phase))
+        mkpath(dirname(output))
+        command = `ffmpeg -i $(tempdir)/2d_%03d.png -vcodec libx264 -b:v 1600k -pix_fmt yuv420p -y $(output)`
+        run(command)
+    end
+end
+
+function plot_raw3d(s, var; basedir=".", xlim=(-1000, 1000), ylim=(-1000, 1000))
+    @assert var in ["eta", "phi"]
+    dir = joinpath(basedir, @sprintf("%s/%03ddeg", s.casename, s.twist))
+    mktempdir() do tempdir
+        Threads.@threads for i in 1:s.N
+            local fname = joinpath(tempdir, @sprintf("3d_%03d.png", i))
+            local v = NCDataset(joinpath(dir, "ep.nc"))[@sprintf("%s%03d", var, s.phase)][i, :, :]
+            local f = Figure(size=(11cm, 8cm), fontsize=12pt)
+            local ax = Axis3(f[1, 1],
+                title="Time=$(i)s",
+                xlabel="x (m)",
+                ylabel="y (m)",
+                zlabel="z (m)",
+                azimuth=-pi / 4,
+                elevation=pi / 3,
+            )
+            xlims!(ax, -1000, 1000)
+            ylims!(ax, -1000, 1000)
+            zlims!(ax, -s.amp, s.amp)
+            local sf = surface!(ax,
+                -s.Lx/2:s.dx:s.Lx/2,
+                -s.Ly/2:s.dy:s.Ly/2,
+                v,
+                colormap=:deepsea,
+                colorrange=(-s.amp * 1.2, s.amp * 1.2),
+            )
+            Colorbar(f[1, 2], sf)
+            save(fname, f)
+        end
+        output = joinpath(dir, @sprintf("video/%s_3d_%03d.mp4", var, s.phase))
+        mkpath(dirname(output))
+        command = `ffmpeg -i $(tempdir)/3d_%03d.png -vcodec libx264 -b:v 1600k -pix_fmt yuv420p -y $(output)`
+        run(command)
+    end
+end
+function plot_4p(s, var, combination; basedir=".")
+    @assert var in ["eta", "phi"]
+    @assert combination in 1:4
+    if combination == 1
+        mult = 1
+    else
+        mult = 0.1
+    end
+    vlim = s.amp * mult
+    dir = @sprintf("%s/%03ddeg", s.casename, s.twist)
+    mktempdir() do tempdir
+        Threads.@threads for i in 1:s.N
+            local fname = joinpath(tempdir, @sprintf("4p%d_%03d.png", combination, i))
+            local fp = NCDataset(joinpath(dir, "4p.nc"))[@sprintf("%s%d", var, combination)][i, :, :]
+            local f = Figure(size=(11cm, 8cm), fontsize=12pt)
+            local ax = Axis3(f[1, 1],
+                title="Time=$(i)s",
+                xlabel="x (m)",
+                ylabel="y (m)",
+                zlabel="z (m)",
+                azimuth=-pi / 4,
+                elevation=pi / 3,
+            )
+            xlims!(ax, -1000, 1000)
+            ylims!(ax, -1000, 1000)
+            zlims!(ax, -vlim, vlim)
+            local sf = surface!(ax,
+                -s.Lx/2:s.dx:s.Lx/2,
+                -s.Ly/2:s.dy:s.Ly/2,
+                fp,
+                colormap=:deepsea,
+                colorrange=(-vlim, vlim),
+            )
+            Colorbar(f[1, 2], sf)
+            save(fname, f)
+        end
+        output = joinpath(dir, "video/4p$combination.mp4")
+        mkpath(dirname(output))
+        command = `ffmpeg -i /home/kosuke/ow3d/$(s.casename)/$(@sprintf("%03d",s.twist))deg/harmonics/4p$(combination)_%03d.png -vcodec libx264 -b:v 1600k -pix_fmt yuv420p -y $(output)`
+        run(command)
+    end
+end
